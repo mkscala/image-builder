a766fed6c1211f08e5ef22cf9525d9258bb7f87a
Bryan Kendall <bryan@runnable.com>
2014-08-28 16:54:13 -0700

    Adding tests for circleci

---
+++ scripts/test-circle.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker run \
+  -e RUNNABLE_AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
+  -e RUNNABLE_AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+  -e RUNNABLE_FILES_BUCKET='runnable.image-builder' \
+  -e RUNNABLE_PREFIX='' \
+  -e RUNNABLE_FILES='{ "Dockerfile": "K6cluDupwQdFRsuTPJ0SFUrxUB4lmF_Q" }' \
+  -e RUNNABLE_KEYS_BUCKET='runnable.image-builder' \
+  -e RUNNABLE_DEPLOYKEY='flaming-octo-nemesis.key' \
+  -e RUNNABLE_REPO='git@github.com:bkendall/flaming-octo-nemesis' \
+  -e RUNNABLE_COMMITISH='master' \
+  -e RUNNABLE_DOCKER="$(cat DOCKER_IP):5354" \
+  -e RUNNABLE_DOCKERTAG='test-built-image' \
+  -e RUNNABLE_DOCKER_BUILDOPTIONS='' \
+  test-image-builder
